edd6f273ea7534e99ec62da1d6d9e7d81273632f
Gaston Valles <gastonvallesyeou@gmail.com>
2023-06-27 17:14:08 -0300

    some updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,19 +62,19 @@ modalCloses.forEach((modalClose) => {
     })
 })
 
-let swiper = new Swiper('.portfolio__container', {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    keyboard: true,
-});
+// let swiper = new Swiper('.portfolio__container', {
+//     cssMode: true,
+//     loop: true,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     keyboard: true,
+// });
 
 const sections = document.querySelectorAll('section[id]')
 
